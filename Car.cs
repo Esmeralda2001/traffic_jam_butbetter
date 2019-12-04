b6eedd0b8c4fb9c2b7dfc4e177b7a40cb7a33f60
Selene Blok <20491756+selenecodes@users.noreply.github.com>
2019-12-04 10:33:04 +0100

    Add files via upload

--- Car.cs
+++ Car.cs
@@ -4,51 +4,15 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    public double speed;
-
-    private GameObject road = GameObject.FindGameObjectWithTag("road");
     // Start is called before the first frame update
- 
     void Start()
     {
-        int maxRoadSpeed = road.GetComponent<Road>().maxSpeed;
-        SetInitialSpeed(maxRoadSpeed); 
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void SetInitialSpeed(int max)
-    {
-        int std = 10; // 10km/h was randomly chosen because we do not know how big the differences in speed are between drivers exactly
-        int averageSpeed = max - std; // On average not everyone will be exactly at the speed limit. People prefer to stay a little below it
-        int minSpeed = max - 20; // Australia does not allow one to drive more than 20km/h below the speed limit
-
-        System.Random rand = new System.Random();
-
-        double u1 = 1.0 - rand.NextDouble();
-        double u2 = 1.0 - rand.NextDouble();
-        double randStdNormal = System.Math.Sqrt(-2.0 * System.Math.Log(u1)) *
-                         System.Math.Sin(2.0 * System.Math.PI * u2);
-        double randNormal = averageSpeed + std * randStdNormal;
-
-        if (randNormal > max)
-        {
-            this.speed = max;
-        }else if (randNormal < minSpeed)
-        {
-            this.speed = minSpeed;
-        }
-        else
-        {
-            this.speed = randNormal; 
-        }
-
-   
-        print(randNormal);
     }
 }
--- Road.cs
+++ Road.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Road : MonoBehaviour
-{
+{   
+    public GameObject carPrefab;
+    public int lanes = 1;
+    public int carCount = 10;
+    private List<Vector3> spawnLocations = new List<Vector3>();
     // Start is called before the first frame update
-
-    public int maxSpeed = 130;
-
     void Start()
     {
+        setSpawnLocations();
+        spawnCars();
+    }
 
+    void spawnCars() {
+        for (int i = 0; i < carCount; i++)
+        {
+            System.Random rnd = new System.Random();
+            int r = rnd.Next(this.spawnLocations.Count);
+            Vector3 spawnLocation = this.spawnLocations[r];
+            Instantiate(carPrefab, spawnLocation, Quaternion.identity);
+        }
+            
+    }
+
+    void setSpawnLocations() {
+        float groundLevel = 0F;
+        float lanePadding = 6.7F;
+        float startOfLevel = 50F;
+
+        Vector3 midLane = new Vector3(0, groundLevel, startOfLevel);
+        Vector3 rightLane = new Vector3(-lanePadding, groundLevel, startOfLevel);
+        Vector3 leftLane = new Vector3(lanePadding, groundLevel, startOfLevel);
+
+        if (this.lanes == 1)
+        {
+            this.spawnLocations.Add(midLane);
+        }
+
+        if (this.lanes == 2)
+        {
+            this.spawnLocations.Add(leftLane);
+            this.spawnLocations.Add(midLane);
+        }
+
+        if (this.lanes == 3)
+        {
+            this.spawnLocations.Add(leftLane);
+            this.spawnLocations.Add(midLane);
+            this.spawnLocations.Add(rightLane);
+        }
+
+        this.spawnLocations.ForEach(lane => Debug.Log(lane));
     }
 
     // Update is called once per frame
@@ -18,6 +61,4 @@ public class Road : MonoBehaviour
     {
         
     }
-
-    
 }
