a76276ccf85be822b2be87180abbc314fd67bb0b
Esmeralda <esmeralda.dewolf@student.hu.nl>
2019-12-04 10:34:37 +0100

    Car and Road

---
+++ Road.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int maxSpeed = 130;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
